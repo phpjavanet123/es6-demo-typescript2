3459388aa61d046838c55ec83ded80c706ec708d
Roman Satanovskyi <Roman.Satanovskyi@mindgeek.com>
2019-10-07 13:36:16 +0300

    v1

---
+++ app.ts
@@ -0,0 +1,18 @@
+// app.ts
+
+export class Hero {
+    id: number;
+    name: string;
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    myName()
+    {
+        return this.name;
+    }
+  }
+
+  let hero = new Hero('krunal');
+  console.log(hero.myName());
